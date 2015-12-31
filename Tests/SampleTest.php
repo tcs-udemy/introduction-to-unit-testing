966f41f4dfddf4450b44c962334e3dc8118c2dd9
Trevor Sawler <trevor.sawler@gmail.com>
2015-12-31 13:30:45 -0400

    Sample Test

---
+++ Tests/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Acme\Tests;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testIsTrue() {
+        $myvar = true;
+        $this->assertTrue($myvar);
+    }
+
+}
